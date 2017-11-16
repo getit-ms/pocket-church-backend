d236d4758af737cb852616a118561ad3f06e20b3
Gabriel Silva <gafsel@gmail.com>
2017-11-16 01:57:47 -0200

    Ajuste de registro de push

--- servidor/src/main/java/br/gafs/calvinista/servidor/MensagemServiceImpl.java
+++ servidor/src/main/java/br/gafs/calvinista/servidor/MensagemServiceImpl.java
@@ -8,34 +8,26 @@ package br.gafs.calvinista.servidor;
 import br.gafs.calvinista.dao.FiltroDispositivoNotificacao;
 import br.gafs.calvinista.dao.FiltroEmail;
 import br.gafs.calvinista.dao.QueryNotificacao;
-import br.gafs.calvinista.dao.RegisterSentNotifications;
-import br.gafs.calvinista.dto.ContatoDTO;
-import br.gafs.calvinista.dto.FiltroDispositivoNotificacaoDTO;
-import br.gafs.calvinista.dto.FiltroEmailDTO;
-import br.gafs.calvinista.dto.MensagemEmailDTO;
-import br.gafs.calvinista.dto.MensagemPushDTO;
+import br.gafs.calvinista.dto.*;
 import br.gafs.calvinista.entity.NotificationSchedule;
 import br.gafs.calvinista.entity.domain.NotificationType;
 import br.gafs.calvinista.service.MensagemService;
 import br.gafs.calvinista.servidor.mensagem.EmailService;
 import br.gafs.calvinista.servidor.mensagem.NotificacaoService;
 import br.gafs.calvinista.util.MensagemUtil;
-import br.gafs.calvinista.view.View.Resumido;
 import br.gafs.dao.BuscaPaginadaDTO;
 import br.gafs.dao.DAOService;
 import br.gafs.util.date.DateUtil;
 import br.gafs.util.email.EmailUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 /**
  *
@@ -52,6 +44,9 @@ public class MensagemServiceImpl implements MensagemService {
 
     @EJB
     private NotificacaoService notificacaoService;
+
+    @EJB
+    private NotificationScheduleServiceImpl notificationScheduleService;
 
     private ObjectMapper om = new ObjectMapper();
 
@@ -169,36 +164,12 @@ public class MensagemServiceImpl implements MensagemService {
 
     @Override
     public NotificationSchedule sendLater(MensagemPushDTO notificacao, FiltroDispositivoNotificacaoDTO filtro, Date dataHora) {
-        try {
-            ObjectWriter writer = om.writerWithView(Resumido.class);
-
-            NotificationSchedule registro = new NotificationSchedule(
-                    NotificationType.PUSH, dataHora,
-                    writer.writeValueAsString(notificacao),
-                    writer.writeValueAsString(filtro));
-
-            return daoService.create(registro);
-        } catch (IOException ex) {
-            Logger.getLogger(MensagemServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
+        return notificationScheduleService.createPUSH(notificacao, filtro, dataHora);
     }
 
     @Override
     public NotificationSchedule sendLater(MensagemEmailDTO notificacao, FiltroEmailDTO filtro, Date dataHora) {
-        try {
-            ObjectWriter writer = om.writerWithView(Resumido.class);
-
-            NotificationSchedule registro = new NotificationSchedule(
-                    NotificationType.EMAIL, dataHora,
-                    writer.writeValueAsString(notificacao),
-                    writer.writeValueAsString(filtro));
-
-            return daoService.create(registro);
-        } catch (IOException ex) {
-            Logger.getLogger(MensagemServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
+        return notificationScheduleService.createEMAIL(notificacao, filtro, dataHora);
     }
 
 }
---
+++ servidor/src/main/java/br/gafs/calvinista/servidor/NotificationScheduleServiceImpl.java
@@ -0,0 +1,67 @@
+package br.gafs.calvinista.servidor;
+
+import br.gafs.calvinista.dto.FiltroDispositivoNotificacaoDTO;
+import br.gafs.calvinista.dto.FiltroEmailDTO;
+import br.gafs.calvinista.dto.MensagemEmailDTO;
+import br.gafs.calvinista.dto.MensagemPushDTO;
+import br.gafs.calvinista.entity.NotificationSchedule;
+import br.gafs.calvinista.entity.domain.NotificationType;
+import br.gafs.calvinista.view.View;
+import br.gafs.dao.DAOService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import java.io.IOException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Gabriel on 16/11/2017.
+ */
+@Stateless
+@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+public class NotificationScheduleServiceImpl {
+
+    @EJB
+    private DAOService daoService;
+
+    private ObjectMapper om = new ObjectMapper();
+
+    public NotificationSchedule createPUSH(MensagemPushDTO notificacao, FiltroDispositivoNotificacaoDTO filtro, Date dataHora) {
+        try {
+
+            ObjectWriter writer = om.writerWithView(View.Resumido.class);
+
+            NotificationSchedule registro = new NotificationSchedule(
+                    NotificationType.PUSH, dataHora,
+                    writer.writeValueAsString(notificacao),
+                    writer.writeValueAsString(filtro));
+
+            return daoService.create(registro);
+        } catch (IOException ex) {
+            Logger.getLogger(MensagemServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    public NotificationSchedule createEMAIL(MensagemEmailDTO notificacao, FiltroEmailDTO filtro, Date dataHora) {
+        try {
+            ObjectWriter writer = om.writerWithView(View.Resumido.class);
+
+            NotificationSchedule registro = new NotificationSchedule(
+                    NotificationType.EMAIL, dataHora,
+                    writer.writeValueAsString(notificacao),
+                    writer.writeValueAsString(filtro));
+
+            return daoService.create(registro);
+        } catch (IOException ex) {
+            Logger.getLogger(MensagemServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+}
