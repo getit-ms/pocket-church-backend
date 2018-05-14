d01ee663eb427b5fc4b4b4ada45afda45fba960e
Gabriel Silva <gafsel@gmail.com>
2018-05-14 11:54:37 -0300

    Ajustes para melhorar renderizaçao de imagem

--- servidor/src/main/java/br/gafs/calvinista/util/PDFToImageConverterUtil.java
+++ servidor/src/main/java/br/gafs/calvinista/util/PDFToImageConverterUtil.java
@@ -13,7 +13,10 @@ import org.ghost4j.renderer.RendererException;
 import org.ghost4j.renderer.SimpleRenderer;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
 import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -78,9 +81,38 @@ public class PDFToImageConverterUtil {
         private BufferedImage getBufferedImage(PDFDocument document, SimpleRenderer renderer, int i) throws IOException, RendererException, DocumentException {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
-            ImageIO.write((RenderedImage) renderer.render(document, i, i).get(0), "png", baos);
+            Image image = renderer.render(document, i, i).get(0);
 
-            return ImageIO.read(new ByteArrayInputStream(ImageUtil.redimensionaImagem(baos.toByteArray(), "png", LIMIT_HEIGHT, LIMIT_WIDTH)));
+            boolean redimensiona = false;
+            int width = image.getWidth(null);
+            int height = image.getHeight(null);
+
+            if(LIMIT_HEIGHT > 0 && height > LIMIT_HEIGHT) {
+                width = width * LIMIT_HEIGHT / height;
+                height = LIMIT_HEIGHT;
+                redimensiona = true;
+            }
+
+            if(LIMIT_WIDTH > 0 && width > LIMIT_WIDTH) {
+                height = height * LIMIT_WIDTH / width;
+                width = LIMIT_WIDTH;
+                redimensiona = true;
+            }
+
+            return createBufferedImage(
+                    redimensiona ? new ImageIcon(image).getImage().getScaledInstance(width, height, Image.SCALE_REPLICATE) : image,
+                    BufferedImage.TYPE_INT_RGB);
+        }
+
+        private static BufferedImage createBufferedImage(Image imageIn, int imageType) {
+            if(imageIn instanceof BufferedImage) {
+                return (BufferedImage)imageIn;
+            } else {
+                BufferedImage bufferedImageOut = new BufferedImage(imageIn.getWidth((ImageObserver)null), imageIn.getHeight((ImageObserver)null), imageType);
+                Graphics g = bufferedImageOut.getGraphics();
+                g.drawImage(imageIn, 0, 0, (ImageObserver)null);
+                return bufferedImageOut;
+            }
         }
     }
 
