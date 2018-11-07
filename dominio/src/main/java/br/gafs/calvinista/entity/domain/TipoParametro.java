b85e6c715f3a3c6cb49d2a33801d411679847454
Gabriel Silva <gafsel@gmail.com>
2018-11-07 00:37:39 -0200

    Ajustes para parametrizar dados via banco de dados

--- dominio/src/main/java/br/gafs/calvinista/entity/domain/TipoParametro.java
+++ dominio/src/main/java/br/gafs/calvinista/entity/domain/TipoParametro.java
@@ -7,7 +7,6 @@ package br.gafs.calvinista.entity.domain;
 
 import br.gafs.calvinista.entity.Parametro;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.prism.shader.AlphaOne_RadialGradient_AlphaTest_Loader;
 import lombok.*;
 
 import java.lang.annotation.ElementType;
@@ -15,7 +14,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
