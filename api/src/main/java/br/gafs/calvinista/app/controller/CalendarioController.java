0dbdb02faa313e11518826f6e10ea4f8001ccdc9
Gabriel Silva <gafsel@gmail.com>
2019-04-06 00:15:24 -0300

    FIX consulta de calendários

--- api/src/main/java/br/gafs/calvinista/app/controller/CalendarioController.java
+++ api/src/main/java/br/gafs/calvinista/app/controller/CalendarioController.java
@@ -39,7 +39,7 @@ public class CalendarioController {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response busca(
-            @QueryParam("pagina") final Integer pagina,
+            @QueryParam("pagina") @DefaultValue("1") final Integer pagina,
             @QueryParam("total") @DefaultValue("10") final Integer total){
         return Response.status(Response.Status.OK)
                 .entity(appService.buscaEventos(pagina, total)).build();
