bb60d3b5730288b06a1d9096a3d97b13f287103b
Gabriel Silva <gafsel@gmail.com>
2019-09-09 13:41:34 -0300

    Ajustes finais para nova versão

--- api/src/main/java/br/gafs/pocket/corporate/app/controller/infra/EmpresaController.java
+++ api/src/main/java/br/gafs/pocket/corporate/app/controller/infra/EmpresaController.java
@@ -5,13 +5,8 @@
  */
 package br.gafs.pocket.corporate.app.controller.infra;
 
-import br.gafs.pocket.corporate.app.util.MergeUtil;
 import br.gafs.pocket.corporate.dto.FiltroEmpresaDTO;
-import br.gafs.pocket.corporate.dto.ParametrosEmpresaDTO;
-import br.gafs.pocket.corporate.entity.Empresa;
-import br.gafs.pocket.corporate.service.AdminService;
-import br.gafs.pocket.corporate.service.ParametroService;
-import br.gafs.pocket.corporate.view.View;
+import br.gafs.pocket.corporate.service.AppService;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
@@ -24,73 +19,40 @@ import javax.ws.rs.core.Response;
  * @author Gabriel
  */
 @RequestScoped
-@Path("infra/empresa")
+@Path("empresa")
 public class EmpresaController {
-    
+
     @EJB
-    private AdminService adminService;
-    
-    @EJB
-    private ParametroService paramService;
+    private AppService appService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response busca(
             @QueryParam("chave") String chave,
             @QueryParam("filtro") String filtro,
-            @QueryParam("pagina") @DefaultValue("1") final Integer pagina,
-            @QueryParam("total") @DefaultValue("10") final Integer total){
-        return Response.status(Response.Status.OK).entity(adminService.busca(new FiltroEmpresaDTO(chave, filtro, pagina, total))).build();
+            @QueryParam("pagina") @DefaultValue("1") Integer pagina,
+            @QueryParam("tamanho") @DefaultValue("10") Integer tamanho
+    ) {
+        return Response.status(Response.Status.OK).entity(appService.busca(
+                new FiltroEmpresaDTO(chave, filtro, pagina, tamanho))).build();
     }
-    
+
+
     @GET
-    @Path("{empresa}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@PathParam("empresa") String empresa){
-        return Response.status(Response.Status.OK).entity(adminService.buscaEmpresa(empresa)).build();
-    }
-    
-    @POST
+    @Path("template")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response cadatra(final Empresa empresa){
-        adminService.cadastra(empresa);
-        return Response.status(Response.Status.OK).build();
+    public Response buscaTemplate() {
+        return Response.status(Response.Status.OK).entity(appService.buscaTemplate()).build();
     }
-    
-    @PUT
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response atualiza(final Empresa empresa){
-        Empresa entidade = adminService.buscaEmpresa(empresa.getChave());
-        MergeUtil.merge(empresa, View.Edicao.class).into(entidade);
-        adminService.atualiza(entidade);
-        return Response.status(Response.Status.OK).build();
-    }
-    
-    @PUT
-    @Path("{empresa}/parametros")
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response atualiza(@PathParam("empresa") String id, final ParametrosEmpresaDTO parametros){
-        paramService.salvaParametros(parametros, id);
-        return Response.status(Response.Status.OK).build();
-    }
-    
+
     @GET
-    @Path("{empresa}/parametros")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response atualiza(@PathParam("empresa") String id){
-        return Response.status(Response.Status.OK).entity(paramService.buscaParametros(id)).build();
-    }
-    
-    @DELETE
-    @Path("{empresa}")
+    @Path("template-app")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response remove(@PathParam("empresa") Long empresa){
-        adminService.inativa(empresa);
-        return Response.status(Response.Status.OK).build();
+    public Response buscaTemplateApp() {
+        return Response.status(Response.Status.OK).entity(appService.buscaTemplateApp()).build();
     }
-    
+
 }
