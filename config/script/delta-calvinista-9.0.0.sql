7a947135ef1f9f97a67b1de926393cb3c00ba4de
Gabriel Silva <gabriel@mainclass.com.br>
2021-08-20 23:11:34 -0300

    FIX aponamento de entidades

--- config/script/delta-calvinista-9.0.0.sql
+++ config/script/delta-calvinista-9.0.0.sql
@@ -1,9 +1,10 @@
 create table tb_termo_aceite
 (
-    id_termo_aceite bigint      not null primary key,
+    id_termo_aceite bigint      not null,
     chave_igreja    varchar(50) not null,
     termo           text        not null,
     versao          integer     not null,
+    primary key (id_termo_aceite, chave_igreja),
     constraint fk_igreja_termo_aceite
         foreign key (chave_igreja)
             references tb_igreja (chave_igreja)
@@ -29,3 +30,10 @@ create table rl_termo_aceite_membro
         foreign key (chave_igreja)
             references tb_igreja (chave_igreja)
 );
+
+insert into rl_plano_funcionalidade(id_plano, funcionalidade)
+select id_plano, 'MANTER_TERMO_ACEITE' from tb_plano where id_plano in (3,1,2,4,5,6,7,8952);
+
+insert into rl_perfil_funcionalidade(id_perfil, chave_igreja, funcionalidade)
+select id_perfil, chave_igreja, 'MANTER_TERMO_ACEITE' from tb_perfil
+where nome = 'Administrador' and chave_igreja in (select chave_igreja from tb_igreja where id_plano in (3,1,2,4,5,6,7,8952));
